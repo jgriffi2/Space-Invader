cecaf0ecb98b1d189190196248c76a2fa7d7fc18
William Du <william.a.du@gmail.com>
2016-11-09 15:47:24 -0800

    Completes user input for shooting, moving and quiting

--- user-input.py
+++ user-input.py
@@ -1,28 +1,36 @@
+# pip install serial
+# pip install readchar
+
 import serial 
 import readchar
 
 class UserInputController(object):
   def __init__(self):
-    ser = serial.Serial('/dev/cu.usbserial-ADAPFFKjO', 9600)
+    self.ser = serial.Serial('/dev/cu.usbserial-ADAPFFKjO', 9600)
 
   def runInputLoop(self):
     while True:
       char = repr(readchar.readkey())
-      if char == '\\x1b[A' or char == 'w':
+      if char == "'\\x1b[A'" or char == "'w'":
         self.ser.write('U')
-      elif char == '\\x1b[C' or char == 'd':
+      elif char == "'\\x1b[C'" or char == "'d'":
         self.ser.write('R')
-      elif char == '\\x1b[D' or char == 'a':
+      elif char == "'\\x1b[D'" or char == "'a'":
         self.ser.write('L')
-      elif char == '\\x1b[B' or char == 's':
+      elif char == "'\\x1b[B'" or char == "'s'":
         self.ser.write('D')
-      else char == 'q':
-        self.ser.write('D')
+      elif char == "' '":
+        self.ser.write('S')
+      elif char == "'q'":
+        self.ser.write('Q')
         return
+      else:
+        continue
 
 def main():
   user_input = UserInputController()
   user_input.runInputLoop()
+  return
 
 if __name__ == "__main__":
     main()
