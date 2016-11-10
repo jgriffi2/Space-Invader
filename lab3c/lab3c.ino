aed8d732098e23b5ecf78764145e93a7bb3f07d3
jgriffi2 <jgriffi2@stanford.edu>
2016-11-09 16:25:24 -0800

    moved enemies

--- lab3c/lab3c.ino
+++ lab3c/lab3c.ino
@@ -4,6 +4,7 @@ int enemies[maxEnemies][3];
 int velocity;
 int alive = 1;
 byte LEDon[4][4][4];
+bool positive;
 
 // vector of int[3] for enemies, int[3] holds x, y, z positions
 // int[3] for player, holds x, y, z positions
@@ -22,20 +23,20 @@ void setup() {
     pinMode(NEGATIVE_PINS[i], OUTPUT);
     digitalWrite(NEGATIVE_PINS[i], HIGH);
   }
-  
+
   Serial.begin(115200);
   Serial.setTimeout(100);
 }
 
 /* Function: getValue
- * ------------------
- * Returns the value in a 4x4x4 values array, each dimension representing an
- * axis of the LED cube, that corresponds to the given P-wire and N-wire
- * number.
- *
- * This function is called by display(), in order to find whether an LED for a
- * particular P-wire and N-wire should be switched on.
- */
+   ------------------
+   Returns the value in a 4x4x4 values array, each dimension representing an
+   axis of the LED cube, that corresponds to the given P-wire and N-wire
+   number.
+
+   This function is called by display(), in order to find whether an LED for a
+   particular P-wire and N-wire should be switched on.
+*/
 inline byte getValue(byte values[4][4][4], byte pNum, byte nNum)
 {
   byte x;
@@ -54,14 +55,14 @@ inline byte getValue(byte values[4][4][4], byte pNum, byte nNum)
 }
 
 /* Function: display
- * -----------------
- * Runs through one multiplexing cycle of the LEDs, controlling which LEDs are
- * on.
- *
- * During this function, LEDs that should be on will be turned on momentarily,
- * one row at a time. When this function returns, all the LEDs will be off
- * again, so it needs to be called continuously for LEDs to be on.
- */
+   -----------------
+   Runs through one multiplexing cycle of the LEDs, controlling which LEDs are
+   on.
+
+   During this function, LEDs that should be on will be turned on momentarily,
+   one row at a time. When this function returns, all the LEDs will be off
+   again, so it needs to be called continuously for LEDs to be on.
+*/
 void display(byte values[4][4][4])
 {
   for (byte pNum = 0; pNum < 8; pNum++) { // iterate through P-wires
@@ -69,12 +70,12 @@ void display(byte values[4][4][4])
     // Set up all the N-wires first
     for (byte nNum = 0; nNum < 8; nNum++) { // iterate through N-wires
       byte value = getValue(values, pNum, nNum); // look up the value
-      if(value > 0) digitalWrite(NEGATIVE_PINS[nNum], LOW);
+      if (value > 0) digitalWrite(NEGATIVE_PINS[nNum], LOW);
       else digitalWrite(NEGATIVE_PINS[nNum], HIGH);
     }
-    
+
     digitalWrite(POSITIVE_PINS[pNum], LOW);
-    
+
     delayMicroseconds(1000);
 
     digitalWrite(POSITIVE_PINS[pNum], HIGH);
@@ -85,36 +86,44 @@ void display(byte values[4][4][4])
 void loop() {
   // if player is alive
   if (alive) {
-      static byte ledOn[4][4][4];
 
-  if (Serial.available()) {
-    char control = Serial.read();
-    if (control == 'U') {
-      if (player[1] != 3) player[1] = player[1] + 1;
-      
-    } else if (control == 'D') {
-      if (player[1] != 0) player[1] = player[1] - 1;
-      
-    } else if (control == 'L') {
-      if (player[0] != 0) player[1] = player[1] - 1;
-      
-    } else if (control == 'R') {
-      if (player[0] != 3) player[1] = player[1] + 1;
-      
-    } else if (control == 'S') {
-      // check if enemy has same xy coords (destroy if so)
-      destroyEnemies();
-    } else if (control == 'Q') {
-      // GET OUT 
+    resetLEDs();
+
+    byte x = 0;
+    byte y = 0;
+    byte z = 0;
+    static char message[60];
+
+    if (Serial.available()) {
+      char control = Serial.read();
+      if (control == 'U') {
+        if (player[1] != 3) player[1] = player[1] + 1;
+
+      } else if (control == 'D') {
+        if (player[1] != 0) player[1] = player[1] - 1;
+
+      } else if (control == 'L') {
+        if (player[0] != 0) player[1] = player[1] - 1;
+
+      } else if (control == 'R') {
+        if (player[0] != 3) player[1] = player[1] + 1;
+
+      } else if (control == 'S') {
+        // check if enemy has same xy coords (destroy if so)
+        destroyEnemies();
+      } else if (control == 'Q') {
+        resetLEDs();
+        alive = 0;
+      }
     }
-  }
     // listen for keys for movement
     // listen for keys for shooting
-    // move player if moved
     // move enemies
+    if (checkDirection()) moveEnemiesDown();
+    else moveEnemiesSide();
     // check if enemies are at base
     // set alive accordingly
-    
+
   } else {
     // prompt user to begin
   }
@@ -125,6 +134,43 @@ static void destroyEnemies() {
     if (player[0] == enemy[0] && player[1] == enemy[1]) {
       LEDon[enemy[0]][enemy[1]][enemy[2]] = 0;
     }
+  }
+}
+
+static void resetLEDs() {
+  for (int x = 0; x < 4; x++) {
+    for (int y = 0; y < 4; y++) {
+      for (int z = 0; z < 4; z++) {
+        LEDon[x][y][z] = 0;
+      }
+    }
+  }
+}
+
+static void checkDirection() {
+  for (int[3] enemy : enemies) {
+    if (positive) {
+      if (enemy[0] == 3) positive = !positive;
+    } else {
+      if (enemy[0] == 0) positive = !positive;
+    }
+  }
+}
+
+static void moveEnemiesDown() {
+  for (int[3] enemy : enemies) {
+    enemy[2] = enemy[2] - 1;
+    if (enemy[2] == 0) {
+      alive--;
+      reset_board();
+    }
+  }
+}
+
+static void moveEnemiesSide() {
+  for (int[3] enemy: enemies) {
+    if (positive && enemy[0] != 3) enemy[0] = enemy[0] + 1;
+    else if (!positive && enemy[0] != 0) enemy[0] = enemy[0] - 1;
   }
 }
 
